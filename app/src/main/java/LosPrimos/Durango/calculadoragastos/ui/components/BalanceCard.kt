f83b0a1fb0e41cdeed6012380355ac0ddb893c9a
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2026-04-09 04:41:33 -0700

    Se crearon 3 componentes, una card para poder visualizar en la pantalla de home todos los componentes previos creados donde el usuario puede verificar saldo, balance y el total gastado, tambien un boton para anadir movimiento, todo esto fue creado aparte para mas limpieza

---
+++ app/src/main/java/LosPrimos/Durango/calculadoragastos/ui/components/BalanceCard.kt
@@ -0,0 +1,46 @@
+package LosPrimos.Durango.calculadoragastos.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun BalanceCard(gastado: Double, balance: Double){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(24.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = Color(0xFFC7E3FF)
+        )
+    ) {
+        Column(
+            modifier = Modifier.padding(20.dp)
+        ) {
+            SummaryHeader("Resumen mensual")
+            BalanceSection(gastado)
+            BudgetSummary(balance, balance - gastado)
+            ProgressBar(gastado, balance)
+            Spacer(modifier = Modifier.height(20.dp))
+            TransactionButtons {  }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun BalanceCardPreview(){
+    BalanceCard(23.00,2300.00)
+}
