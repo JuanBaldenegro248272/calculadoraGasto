3021967d8745b7dc96f3b43d55472e1bb6d092b5
JuanBaldenegro248272 <juan.rascon248272@potros.itson.edu.mx>
2026-04-10 05:54:31 -0700

    Se creo el componente del gasto row este nos sirve para mostar los gastos en forma de columna como previsualizaciones de los gastos en la pagina principal

--- app/src/main/java/LosPrimos/Durango/calculadoragastos/ui/components/GastoRow.kt
+++ app/src/main/java/LosPrimos/Durango/calculadoragastos/ui/components/GastoRow.kt
@@ -1,2 +1,85 @@
 package LosPrimos.Durango.calculadoragastos.ui.components
 
+import LosPrimos.Durango.calculadoragastos.utils.formatCurrency
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ShoppingCart
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun GastoRow(nombre: String,
+             fecha: String,
+             monto: Double,
+             icono: ImageVector){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 6.dp),
+        shape = RoundedCornerShape(20.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFADD8FF)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 3.dp)
+    ){
+        Row(
+            modifier = Modifier
+                .padding(16.dp)
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ){
+            Column(
+                modifier = Modifier.weight(1f)
+            ) {
+                Text(
+                    text = nombre,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+                Text(
+                    text = fecha,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.Black
+                )
+            }
+            Text(
+                modifier = Modifier
+                    .padding(horizontal = 20.dp),
+                text = formatCurrency(monto),
+                fontSize = 20.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = Color.Black
+            )
+            Icon(
+                imageVector = icono,
+                contentDescription = null,
+                modifier = Modifier.size(28.dp)
+            )
+        }
+    }
+}
+
+@Composable
+@Preview
+fun GastoRowPreview(){
+    Column(modifier = Modifier.padding(10.dp)) {
+        GastoRow("Marisquitos", "Ayer", 120.0, Icons.Default.ShoppingCart)
+        GastoRow("Hola", "Hoy", 120.0, Icons.Default.ShoppingCart)
+    }
+}
